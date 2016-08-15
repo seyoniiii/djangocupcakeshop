8508ec57ae90ee9b223fe1d0aa37d99c0ba2804a
seyoniiii <sylee22619@gmail.com>
2016-08-15 17:58:01 +0900

    2016 pycon djangogirls cupcakeshop

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class  Cupcake(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=20)
+    rating = models.FloatField()
+    image = models.ImageField(upload_to ='images/cakes')
+    writer = models.ForeignKey(User)
+    createdAt = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
